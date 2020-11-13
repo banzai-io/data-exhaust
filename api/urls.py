2250a6bc6a5aa5d281559bf5bc7fa9433131bd18
virbose <vlad.birtocian@banzai.io>
2020-11-13 15:22:56 +0000

    API Docs included

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,24 @@
+from django.conf.urls import url
 from django.urls import include, path
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.routers import DefaultRouter
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from rest_framework import permissions
 from api.views.internal import banzai
 from api.views.external import provider
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Data Exhaust API",
+      default_version='v1',
+      description="Includes information about the public and private API endpoints",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny, ),
+)
 
 
 router = DefaultRouter()
@@ -9,5 +26,8 @@ router.register(r'data-signals-internal', banzai.PrivateDataSignalViewSet)
 router.register(r'data-signals', provider.PublicDataSignalViewSet)
 
 urlpatterns = [
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('', include(router.urls)),
 ]
