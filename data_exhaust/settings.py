ec6fc527107b85a9c0f29ae0d00bc1d8e2d22c39
Konstantin Pavlovsky <konstantin.pavlovsky@gmail.com>
2020-11-16 14:01:14 +0100

    allowed hosts

--- data_exhaust/settings.py
+++ data_exhaust/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
 
 # Application definition
 
